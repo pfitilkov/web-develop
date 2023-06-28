1cc202387c33a967e767a47defb612f1a76e5c65
Pavel Fitilkov <135705462+pfitilkov@users.noreply.github.com>
2023-06-28 20:46:13 +0300

    Update BingBot_FitilkovPS_task2.js

--- BingBot_FitilkovPS_task2.js
+++ BingBot_FitilkovPS_task2.js
@@ -5,19 +5,48 @@
 // @description  Bing search bot
 // @author       Pavel Fitilkov
 // @match        https://www.bing.com/*
-// @match        https://auto.ru/*
+// @match        https://napli.ru/*
+// @match        https://kiteuniverse.ru/*
+// @match        https://motoreforma.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
 // ==/UserScript==
 
-
+let sites = {
+"napli.ru": ["DevTools для разработчика",
+"Установка и настройка Git, Node JS",
+"Отключение редакций",
+"Вывод произвольных типов записей wp",
+"Плагины VS Сode",
+],
+"kiteuniverse.ru": ["Kite Universe Россия",
+"«Красота. Грация. Интеллект»",
+"Наши воздушные змеи",
+],
+"motoreforma.com": ["прошивки для CAN-AM",
+"тюнинг для квадроциклов BRP",
+"вариатор CV-Tech купить",
+]
+}
+let site = Object.keys(sites)[getRandom(0, Object.keys(sites).length)]; //получили случайный сайт из объекта
 
 let links = document.links;
 let searchButton = document.getElementsByName("search")[0];
-let keywords = ["Максискутер Sym Maxym TL500", "Yamaha T MAX 560", "Купить автомобиль Geely Monjaro", "Купить VolksWagen", "Купить BMW", "BYD", "Купить авто", "Купить подержанные авто", "Мотоциклы на авто.ру"];
+let keywords = sites[site]; //получили набор ключевых фраз для конкретного сайта
 let keyword = keywords [getRandom(0, keywords.length)];
 let bingInput = document.getElementsByName("q")[0];
 
+//Работаем с cookie
+if (searchButton != undefined) {
+document.cookie = "site=" + site;
+} else if (location.hostname == "www.bing.com") {
+  site = getCookie("site");
+} else {
+  site = location.hostname; 
+
+} 
+
+//Рабоатем на главной странице поисковика
 if (searchButton != undefined) {
 let i = 0;
 let timerId = setInterval(function() {
@@ -31,7 +60,7 @@ let timerId = setInterval(function() {
 
 
 //Работаем на целевом сайте
-} else if (location.hostname == "auto.ru") {
+} else if (location.hostname == site) {
 console.log("Мы на целевом сайте!");
 
 //Работаем на страницах поисковой выдачи
@@ -43,9 +72,9 @@ if (getRandom(0, 101) >= 70) {
    location.href = "https://www.bing.com/";
 }
 if (links.length == 0) {
-location.href = "https://auto.ru/";
+location.href = site;
 }
-else if (links[index].href.indexOf("auto.ru") != -1) {
+else if (links[index].href.indexOf(site) != -1) {
 links[index].click()
 }
 }, getRandom(3500, 5500));
@@ -53,7 +82,7 @@ links[index].click()
 } else {
 let nextBingPage = true;
 for (let i = 0; i < links.length; i++) {
-if (links[i].href.includes("auto.ru")) {
+if (links[i].href.includes(site)) {
 let link = links[i];
 nextBingPage = false;
 console.log("Нашёл строку" + link);
@@ -65,9 +94,9 @@ break;
   }
 }
 let elementExist = setInterval(() => {
-let element = document.querySelector(".sb_bp");
+let element = document.querySelector(".sb_pagS");
 if (element != null) {
-if (document.querySelector(".sb_bp").innerText == "5") {
+if (element.innerText == "5") {
   nextBingPage = false;
   location.href = "https://www.bing.com/";
 } 
@@ -75,21 +104,25 @@ if (document.querySelector(".sb_bp").innerText == "5") {
 clearInterval(elementExist);
 }
 
-}, 150)
+}, 100)
 
-if (document.querySelector(".sb_bp").innerText == "5") {
-  nextBingPage = false;
-  location.href = "https://www.bing.com/";
-}
-if (nextBingPage) {
-setTimeout (() => {
-document.querySelector(".sw_next").click();
-}, getRandom (5000, 7000))
+ if (nextBingPage) {
+    setTimeout(() => {
+      document.querySelector(".sw_next").click();
+    }, getRandom(5000, 7000));
+  }
 }
 
-}
+
 
 
 function getRandom(min, max) {
 return Math.floor(Math.random() * (max - min) + min);
+}
+
+function getCookie(name) {
+let matches = document.cookie.match(new RegExp(
+"(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+));
+return matches ? decodeURIComponent(matches[1]) : undefined;
 }
